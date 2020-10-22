59f4116f5fec0af2c33fdb78de3b659301698231
rc-rohan <rohan6hawat@gmail.com>
2020-10-23 02:53:29 +0530

    Added the dropdown with all the country name list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "America"]);
+
+  useEffect(() => {
+    const getCountryData = async () => {
+      await fetch(" https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //name of the country
+            value: country.countryInfo.iso2, //short code of the country
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountryData();
+  }, []);
+
   return (
     <div className="app">
-      Covid Tracker
+      <div className="app__header">
+        <h1>CoVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value='worldwide'>Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
